d75b080ba4331595173d2f68604a6489f487979a
Tony <380996055598af@gmail.com>
2017-07-06 22:56:00 +0300

    Created solution for task 26

---
+++ solution.task0126.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task01.task0126;
+
+/* 
+Не хочешь  - решай нехотя
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = "Хочешь - решай задачи, не хочешь - решай нехотя";
+        System.out.println(text); 
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+    }
+}
