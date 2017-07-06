cf4138b77f5f35183ec79a3a7dcb05364a40a96d
Tony <380996055598af@gmail.com>
2017-07-06 22:45:54 +0300

    Created solution for task 38

---
+++ solution.task0138.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("");
+        System.out.println("1234567890");
+        System.out.println("1234567890");
+        System.out.println("1234567890");
+        System.out.println("1234567890");
+        System.out.println("1234567890");
+        //напишите тут ваш код
+    }
+}
