b2f8acccae1fed6d1490246045622dd97a8a5720
Tony <380996055598af@gmail.com>
2017-07-09 12:46:10 +0300

    Created solution for task 27

---
+++ solution.task0127.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task01.task0127;
+
+/* 
+Число в квадрате
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = sqr(5);
+        System.out.println(a);
+            }
+
+    public static int sqr(int a) {
+        return a * a;
+      
+    }
+}
